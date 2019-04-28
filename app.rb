1a0ce6512a21d3270e1d6f781a85f6af8509900e
Marat <love@marat.dev>
2019-04-28 22:50:49 +0300

    First version of the script

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require "rubygems"
 require "bundler/setup"
+require "dropbox_api"
+require "pry-byebug"
 
+file_name = "#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
+file_path = File.join(Dir.pwd, "shots", file_name)
+
+imagesnap_command = `imagesnap -w 3 #{file_path}`
+
+file_content = IO.read(file_path)
+
+dropbox_client = DropboxApi::Client.new
+dropbox_client.upload("/#{file_name}", file_content)
