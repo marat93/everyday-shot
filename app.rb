e53c783d5821591447fefc6633c554b2f309a820
Marat <love@marat.dev>
2019-04-28 23:14:10 +0300

    Fix glitches on images from imagesnap

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require "pry-byebug"
 file_name = "#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
 file_path = File.join(Dir.pwd, "shots", file_name)
 
-imagesnap_command = `imagesnap -w 3 #{file_path}`
+imagesnap_command = `zsh -c 'imagesnap -w 3 #{file_path}'`
 
 file_content = IO.read(file_path)
 
