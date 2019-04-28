3d10d69fa80c554d8d365ba9e6ee79faf1d62afe
Marat <love@marat.dev>
2019-04-28 23:23:02 +0300

    Fix glitches on images from imagesnap

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ require "pry-byebug"
 file_name = "#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
 file_path = File.join(Dir.pwd, "shots", file_name)
 
-imagesnap_command = `zsh -c 'imagesnap -w 3 #{file_path}'`
+imagesnap_command = `imagesnap -w 3 #{file_path}`
 
-file_content = IO.read(file_path)
+file_content = File.open(file_path, 'rb') { |file| file.read }
 
 dropbox_client = DropboxApi::Client.new
 dropbox_client.upload("/#{file_name}", file_content)
